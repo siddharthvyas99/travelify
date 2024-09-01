a6613ad2490c9f4b827dfe36571370159b3b0f8d
Siddharth Vyas <siddharth.vyas9299@gmail.com>
2024-09-01 22:35:48 +0530

    Removed redundant import for supbase client

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,7 +1,7 @@
 // pages/login.js
 "use client"
 import { useState } from 'react';
-import supabase from '../../lib/supabaseClient';
+// import supabase from '../../lib/supabaseClient';
 import { useRouter } from 'next/navigation';
 import cityPalace from "./../../public/images/city-palace.jpg";
 
